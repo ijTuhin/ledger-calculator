876daf01cb436210c750919a58d3bd4d6ecaf687
ijTuhin <isratishu0209@gmail.com>
2022-02-17 03:34:18 +0600

    converted the input values of Balance section into float type

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 document.getElementById('calculate-balance').addEventListener('click', function () {
     // console.log('Calculate Button clicked');
     const inputIncome = document.getElementById('inputIncome').value;
+    const inputIncomeValue = parseFloat(inputIncome);
+
     const inputFood = document.getElementById('inputFood').value;
+    const inputFoodValue = parseFloat(inputFood);
+
     const inputRent = document.getElementById('inputRent').value;
+    const inputRentValue = parseFloat(inputRent);
+
     const inputClothes = document.getElementById('inputClothes').value;
-    // console.log(inputIncome, inputFood, inputRent, inputClothes);
+    const inputClothesValue = parseFloat(inputClothes);
+    // console.log(inputIncomeValue, inputFoodValue, inputRentValue, inputClothesValue);
 })
