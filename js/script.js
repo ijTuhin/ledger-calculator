e247c11d94333090d481b9bcb26cbd0a9d4e21ba
ijTuhin <isratishu0209@gmail.com>
2022-02-17 03:51:13 +0600

    cleared input field after showing output

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 document.getElementById('calculate-balance').addEventListener('click', function () {
-    // console.log('Calculate Button clicked');
-    const inputIncome = document.getElementById('inputIncome').value;
-    const inputIncomeValue = parseFloat(inputIncome);
+    // getting input values and converting into float type
+    const inputIncome = document.getElementById('inputIncome');
+    const inputIncomeText = inputIncome.value;
+    const inputIncomeValue = parseFloat(inputIncomeText);
 
-    const inputFood = document.getElementById('inputFood').value;
-    const inputFoodValue = parseFloat(inputFood);
+    const inputFood = document.getElementById('inputFood');
+    const inputFoodText = inputFood.value;
+    const inputFoodValue = parseFloat(inputFoodText);
 
-    const inputRent = document.getElementById('inputRent').value;
-    const inputRentValue = parseFloat(inputRent);
+    const inputRent = document.getElementById('inputRent');
+    const inputRentText = inputRent.value;
+    const inputRentValue = parseFloat(inputRentText);
 
-    const inputClothes = document.getElementById('inputClothes').value;
-    const inputClothesValue = parseFloat(inputClothes);
-    // console.log(inputIncomeValue, inputFoodValue, inputRentValue, inputClothesValue);
+    const inputClothes = document.getElementById('inputClothes');
+    const inputClothesText = inputClothes.value;
+    const inputClothesValue = parseFloat(inputClothesText);
+    // Adding all expenses
+    const totalExpenses = inputFoodValue + inputRentValue + inputClothesValue;
+    console.log(totalExpenses);
+    // Displaying total expenses
+    document.getElementById('total-expenses').innerText = totalExpenses;
+    // Getting input fields cleared
+    inputIncome.value = '';
+    inputFood.value = '';
+    inputRent.value = '';
+    inputClothes.value = '';
 })
