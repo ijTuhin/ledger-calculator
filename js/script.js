98668cb760e79dd070208b0e78b9dc8ce05103d9
ijTuhin <isratishu0209@gmail.com>
2022-02-17 03:28:41 +0600

    getting all four input values of balance section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 document.getElementById('calculate-balance').addEventListener('click', function () {
-    console.log('Calculate Button clicked');
+    // console.log('Calculate Button clicked');
+    const inputIncome = document.getElementById('inputIncome').value;
+    const inputFood = document.getElementById('inputFood').value;
+    const inputRent = document.getElementById('inputRent').value;
+    const inputClothes = document.getElementById('inputClothes').value;
+    // console.log(inputIncome, inputFood, inputRent, inputClothes);
 })
