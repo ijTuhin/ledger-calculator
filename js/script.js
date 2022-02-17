09c0bc5e9b343a238a23c8e0af1c2e8cb80fd092
ijTuhin <isratishu0209@gmail.com>
2022-02-17 12:39:59 +0600

    calculations are done in js

--- js/script.js
+++ js/script.js
@@ -15,18 +15,19 @@ document.getElementById('calculate-balance').addEventListener('click', function 
     const inputClothes = document.getElementById('inputClothes');
     const inputClothesText = inputClothes.value;
     const inputClothesValue = parseFloat(inputClothesText);
-    // Adding all expenses and Displaying total expenses
+    // Adding all expenses and calculating balance amount
     const totalExpenses = inputFoodValue + inputRentValue + inputClothesValue;
-    document.getElementById('total-expenses').innerText = totalExpenses;
-    // Calculating and displaying balance amount
     const balanceAmount = inputIncomeValue - totalExpenses;
+    // displaying outputs
+    document.getElementById('total-expenses').innerText = totalExpenses;
     document.getElementById('balance-amount').innerText = balanceAmount;
     // Getting input fields cleared
-    inputIncome.value = '';
-    inputFood.value = '';
-    inputRent.value = '';
-    inputClothes.value = '';
+    // inputIncome.value = '';
+    // inputFood.value = '';
+    // inputRent.value = '';
+    // inputClothes.value = '';
 })
+
 //For Savings section
 document.getElementById('savings-button').addEventListener('click', function () {
 
@@ -34,16 +35,35 @@ document.getElementById('savings-button').addEventListener('click', function () 
     const inputSavings = document.getElementById('inputSavings');
     const inputSavingsText = inputSavings.value;
     const inputSavingsValue = parseFloat(inputSavingsText);
-    // Calculating percentage
-    const percentageValue = inputSavingsValue / 100;
-    // calculating savings amount from income
+    // Inputs from balance section
     const inputIncome = document.getElementById('inputIncome');
     const inputIncomeText = inputIncome.value;
     const inputIncomeValue = parseFloat(inputIncomeText);
-    const savingsAmount = inputIncomeValue * percentageValue;
-    document.getElementById('savings-amount').innerText = savingsAmount;
 
+    const inputFood = document.getElementById('inputFood');
+    const inputFoodText = inputFood.value;
+    const inputFoodValue = parseFloat(inputFoodText);
+
+    const inputRent = document.getElementById('inputRent');
+    const inputRentText = inputRent.value;
+    const inputRentValue = parseFloat(inputRentText);
+
+    const inputClothes = document.getElementById('inputClothes');
+    const inputClothesText = inputClothes.value;
+    const inputClothesValue = parseFloat(inputClothesText);
+
+    // Adding all expenses and calculating balance amount
+    const totalExpenses = inputFoodValue + inputRentValue + inputClothesValue;
+    const balanceAmount = inputIncomeValue - totalExpenses;
+    // Calculating percentage
+    const percentageValue = inputSavingsValue / 100;
+    // calculating savings and remaining balance amount from income
+    const savingsAmount = inputIncomeValue * percentageValue;
+    const remainingBalance = balanceAmount - savingsAmount;
+    // Displaying output
+    document.getElementById('savings-amount').innerText = savingsAmount;
+    document.getElementById('remaining-balance').innerText = remainingBalance;
     // Getting input fields cleared
-    inputSavings.value = '';
-    inputIncome.value = '';
+    // inputSavings.value = '';
+    // inputIncome.value = '';
 })
